ef8d96db4d99f5149e7b4cc721eb54d10197389c
John Christy <jchristy40@gmail.com>
2024-01-30 13:52:13 -0600

    new problem

--- 387.first-unique-character-in-a-string.ts
+++ 387.first-unique-character-in-a-string.ts
@@ -7,6 +7,24 @@
 // @lc code=start
 function firstUniqChar(s: string): number {
     
+    let charMap = {};
+    let index = 0
+    for (let char of s) {
+        charMap[char] = [charMap[char]+1 || 1, index]
+        index+=1
+        
+
+        
+    }
+
+    for (let key in charMap) {
+        
+        if (charMap[key][0] === 1) {
+            
+            return charMap[key][1]
+
+        }
+    }
+    return -1
 };
 // @lc code=end
-
